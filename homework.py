fb657fe8e065e2172c019ca1116a3315fea53787
Алексей Николаев <cei4bio@gmail.com>
2022-07-15 16:22:16 +0600

    7th.2nd.

--- homework.py
+++ homework.py
@@ -51,44 +51,43 @@ def get_api_answer(current_timestamp):
         response = requests.get(
             ENDPOINT, headers=HEADERS, params=params)
         logger.info('Запрос к API отправлен.')
-        if response.status_code != HTTPStatus.OK:
-            status = response.status_code
-            logger.error(
-                f'Сбой в работе программы: Эндпоинт {response}.'
-                f'Недоступен. Код ответа: {status}')
-            raise exceptions.ConnectionError
     except telegram.TelegramError as error:
         raise telegram.TelegramError(f'Ошибка в API:{error}')
+    else:
+        if response.status_code != HTTPStatus.OK:
+            status = response.status_code
+            raise exceptions.ConnectionError(
+                f'Сбой в работе программы: Эндпоинт {response}.'
+                f'Недоступен. Код ответа: {status}')
     response = response.json()
     return response
 
 
 def check_response(response):
     """Функция проверки данных от API.
+
     Формирование словаря с последней домашкой.
     """
     if not isinstance(response, dict):
-        logger.error('API возвращает не словарь.')
         raise TypeError('API возвращает не словарь.')
     if not 'homeworks':
         raise KeyError('Отсутствует ключ homeworks.')
     homework = response.get('homeworks')
     if not isinstance(homework, list):
-        logger.error('Ключ homework - не список.')
         raise TypeError('Ключ homework - не список.')
     return homework
 
 
 def parse_status(homework):
     """Проверка словаря с последней домашкой и формирование сообщения."""
-    if 'homework_name' not in homework:
+    if not 'homework_name' and 'status':
         raise KeyError('Нет ключей в json.')
     homework_name = homework['homework_name']
     homework_status = homework['status']
     verdict = HOMEWORK_VERDICTS[homework_status]
     if homework_status not in HOMEWORK_VERDICTS:
-        logger.error('Отсутствие статуса домашней работы в списке.')
-        raise telegram.TelegramError
+        raise telegram.TelegramError(
+            'Отсутствие статуса домашней работы в списке.')
 
     return f'Изменился статус проверки работы "{homework_name}". {verdict}'
 
@@ -102,19 +101,18 @@ def main():
     """Основная логика работы бота."""
     bot = telegram.Bot(TELEGRAM_TOKEN)
     current_timestamp = int(time.time())
-    old_message = ''
+    old_message = None
     if check_tokens() is False:
-        logger.critical('Проверка токенов провалена.')
         raise exceptions.CriticalException('Проверка токенов провалена.')
     while True:
         try:
             response = get_api_answer(current_timestamp)
             homeworks = check_response(response)
-            homework = homeworks[0]
-            message = parse_status(homework)
-            if old_message != message and len(homeworks) > 0:
+            if homeworks:
+                homework = homeworks[0]
+                message = parse_status(homework)
                 send_message(bot, message)
-                old_message = message
+            current_timestamp = int(time.time())
         except Exception as error:
             message = f'Сбой в работе программы: {error}'
             logger.error(message)
